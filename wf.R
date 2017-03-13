9f9941d6c2fe534a7764cf04d01347104742f368
sm <xhm@umich.edu>
2017-03-13 20:52:31 +0000

    Fixed bug with starting line etc. Moved helpers to inside the function.

--- wf.R
+++ wf.R
@@ -5,8 +5,77 @@ print.waterfall <- function(dt, bar.width = 9,
     if(bar.width < 2) stop("Width too small.")
     if(nchar(as.character(fill)) > 1) fill <- substr(fill, 1, 1)
 
-    dt  <- prepare.dt(dt)
+    dt  <- prepare.dt(as.data.frame(dt))
     nbar <- nrow(dt)
+    if(nbar < 3) stop("Less then 3 bars. Invalid input.")
+    
+    
+    # ------ HELPERS ------ #
+    # <helpers>
+    # helper function to draw a horizontal line
+    add.hline <- function(width = 9) {
+       return(c("+", rep("-", width - 2), "+"))
+    }
+    
+    # helper function to draw a vertical line
+    add.vline <- function(height = 16) {
+       return(c("+", rep("|", height - 2), "+"))
+    }
+    
+    # helper function to draw a bar
+    add.bar <- function(width = 9, height = 16) {
+       
+       if(height < 2) BAR <- matrix(add.hline(width), nrow = 1)
+       else {
+          BAR <- matrix(" ", ncol = width, nrow = height)
+          BAR[1, 1:width] <- BAR[height, 1:width]<- add.hline(width)
+          BAR[, 1] <- BAR[, width] <- add.vline(height)
+          return(BAR)
+       }
+    }
+    
+    
+    # helper function to prepare data input for waterfall
+    # decode 'e', add tag for 'e' and cumsum for 'e' bars
+    # 'e' bars are just cumsums to this point
+    prepare.dt <- function(dt) {
+       
+       dt[, 2] <- as.character(dt[ ,2])
+       dt$tag <- 0
+       dt$tag[which(dt[, 2] == "e")] <- 1
+       dt[dt$tag == 1, 2] <- 0
+       dt[, 2] <- as.numeric(dt[, 2])
+       dt$cumsum <- cumsum(dt[, 2])
+       dt[, 2][dt$tag == 1]  <- - dt$cumsum[dt$tag == 1]
+       return(dt)
+       
+    }
+    
+    
+    # helper function to wrap texts
+    # start from the second word, then cat the one before
+    # so that if the second one is cat'd to the first one
+    # the next iteration starts from the originally 3rd word
+    cat.prev <- function(words, width) {
+       
+       if (length(words) == 1) return(words)
+       
+       for (i in 2:length(words)) {
+          if (nchar(paste(words[i-1], words[i])) <= width) {
+             words[i] <- paste(words[i-1], words[i])
+             words <- words[-(i-1)]
+          }
+          
+          # if the last word has been cat'd
+          if(is.na(words[i+1])) break
+       }
+       
+       return(words)
+    }
+    
+    # </helpers>
+    
+    # ------- FUNCTION ------ #
 
     # the descriptions
     steps <- as.character(dt[, 1])
@@ -28,11 +97,11 @@ print.waterfall <- function(dt, bar.width = 9,
     # give every bar row names for final binding by rownames
     all.rows <- rownames(WF[[1]])
 
-    for (i in 2:nbar) {
+    for (i in 2:(nbar-1)) {
     	WF[[i]] <- add.bar(bar.width, round(abs(numbers[i]) / x))
 	
 	# if is 'e', then start from bottom, otherwise start from top
-	if(dt$tag[i] == 0)
+	if(dt[i-1 ,2] > 0 | dt$tag[i-1] == 1)
 	    bar.start <- min(as.numeric(rownames(WF[[i-1]])))
 	else
 	    bar.start <- max(as.numeric(rownames(WF[[i-1]])))
@@ -45,10 +114,17 @@ print.waterfall <- function(dt, bar.width = 9,
 	all.rows <- c(all.rows, rownames(WF[[i]]))
     }
 
+    # last bar to reach end coz rounding difference
+    WF[[nbar]] <- add.bar(bar.width, max(as.numeric(all.rows)) - max(as.numeric(rownames(WF[[nbar-1]]))) + 1)
+    rownames(WF[[nbar]]) <- seq(max(as.numeric(rownames(WF[[nbar-1]]))), 
+                                len = nrow(WF[[nbar]]),
+                                by = 1)
+    WF[[nbar]] [WF[[nbar]] == " "] <- fill
+
     # collate all rows
     all.rows <- unique(all.rows)
     all.rows <- all.rows[order(as.numeric(all.rows))]
-
+    
     # set up final chart
     CHART <- matrix(" ", nrow = length(all.rows),
 		    ncol = (bar.width + gap) * nbar + 1)
@@ -68,7 +144,7 @@ print.waterfall <- function(dt, bar.width = 9,
     # combine words if short
     # then strsplit them to fill one letter into one matrix cell
     texts <- strsplit(steps, split = " ")
-    texts <- lapply(texts, abbreviate, min = bar.width - 1, dot = TRUE)
+    texts <- lapply(texts, abbreviate, min = bar.width, dot = FALSE)
     texts <- lapply(texts, cat.prev, bar.width)
     texts <- lapply(texts, format, width = bar.width + gap)
 
@@ -105,70 +181,9 @@ print.waterfall <- function(dt, bar.width = 9,
 
 
 
-# ------ HELPERS ------ #
-# helper function to draw a horizontal line
-add.hline <- function(width = 9) {
-    return(c("+", rep("-", width - 2), "+"))
-}
 
-# helper function to draw a vertical line
-add.vline <- function(height = 16) {
-    return(c("+", rep("|", height - 2), "+"))
-}
-
-# helper function to draw a bar
-add.bar <- function(width = 9, height = 16) {
-
-    if(height < 2) BAR <- matrix(add.hline(width), nrow = 1)
-    else {
-	BAR <- matrix(" ", ncol = width, nrow = height)
-	BAR[1, 1:width] <- BAR[height, 1:width]<- add.hline(width)
-	BAR[, 1] <- BAR[, width] <- add.vline(height)
-	return(BAR)
-    }
-}
-    
-
-# helper function to prepare data input for waterfall
-# decode 'e', add tag for 'e' and cumsum for 'e' bars
-# 'e' bars are just cumsums to this point
-prepare.dt <- function(dt) {
-
-    dt[, 2] <- as.character(dt[ ,2])
-    dt$tag <- 0
-    dt$tag[which(dt[, 2] == "e")] <- 1
-    dt[dt$tag == 1, 2] <- 0
-    dt[, 2] <- as.numeric(dt[, 2])
-    dt$cumsum <- cumsum(dt[, 2])
-    dt[, 2][dt$tag == 1]  <- - dt$cumsum[dt$tag == 1]
-    return(dt)
-
-}
-
-
-# helper function to wrap texts
-# start from the second word, then cat the one before
-# so that if the second one is cat'd to the first one
-# the next iteration starts from the originally 3rd word
-cat.prev <- function(words, width) {
-
-    if (length(words) == 1) return(words)
-
-    for (i in 2:length(words)) {
-	if (nchar(paste(words[i-1], words[i])) <= width) {
-	    words[i] <- paste(words[i-1], words[i])
-	    words <- words[-(i-1)]
-	}
-
-	# if the last word has been cat'd
-	if(is.na(words[i+1])) break
-    }
-
-    return(words)
-}
-
-dt <- data.frame(step = c("Experian White Space", "OC&C Catchments",
-			  "Population Too Low", "Tested",
-			  "Cannibalisation Too High", "B&M White Space"), 
-		 count = c(12, 4, -6, "e", -2, "e")) 
+# dt <- data.frame(step = c("Experian White Space", "OC&C Catchments",
+# 			  "Population Too Low", "Tested",
+# 			  "Cannibalisation Too High", "B&M White Space"),
+# 		 count = c(12, 4, -6, "e", -2, "e"))
 
